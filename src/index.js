2f1502cace1d843edf39d2d0b9585f6034893fb2
VandeurenGlenn <vandeurenglenn@gmail.com>
2018-01-08 21:53:34 +0100

    :neutral_face: Fix daemon error after bad close

--- src/index.js
+++ src/index.js
@@ -1,96 +1,68 @@
 'use strict';
-import {spawn} from 'child_process';
+import { spawn } from 'child_process';
 import { log, stopAndPersist, succes, info, fail } from 'crypto-logger';
 import EventEmitter from 'events';
-import { read, write } from 'crypto-io-utils';
 import { homedir } from 'os';
 import { join } from 'path';
-const emitter = new EventEmitter();
-const home = join(homedir(), '.crypto');
-const daemondir = join(home, 'daemon');
-const lockPath = join(daemondir, '.lock');
+const home = join(homedir(), '.crypto-io');
 const ipfsPath = join(home, 'ipfs/ipfs');
+const emitter = new EventEmitter();
 
-export default class CryptoDaemon extends EventEmitter {
-  constructor() {
-    super();
-    // Bind methods
-    this._onData = this._onData.bind(this);
-    this._onError = this._onError.bind(this);
-    this._onClose = this._onClose.bind(this);
-  }
+let ipfs;
 
-  start() {
-    return new Promise((resolve, reject) => {
-      if (process.env.DEBUG) {
-        log('checking for other daemons');
-      }
-      read(lockPath, 'string').then(value => {
-        if (Boolean(value === 'true')) {
-          // CRYPTODAEMON DAEMON ALREADY RUNNING
-          fail('Another daemon is already running')
-          reject({code: 'CRYPD_DAR', error: 'Another daemon is already running'});
-        } else {
-          this.on('ready', () => resolve());
-          this._start();
-        }
-      }).catch(error => {
-        this.on('ready', () => resolve());
-        this._start();
-      });
-    });
-  }
+const _failsafe = () => {
+  info('removing lock from repo');
+  const unlock = spawn(ipfsPath, ['repo', 'fsck'])
+  unlock.stdout.on('data', data => {
+    info('Restarting Daemon');
+    return start();
+  })
+}
 
-  stop() {
-    write(lockPath, false).then(() => {
-      log('killing Daemon');
-      this.ipfs.kill();
-    });
-  }
+export const on = (name, cb) => {
+  emitter.on(name, cb)
+}
 
-  _start() {
-    log('Starting Daemon');
-    this.ipfs = spawn(ipfsPath, ['daemon']);
-    this.ipfs.stdout.on('data', this._onData);
-    this.ipfs.stderr.on('data', this._onError);
-    this.ipfs.on('close', this._onClose);
-  }
+export const start = () => {
+  log('Starting Daemon');
+  ipfs = spawn(ipfsPath, ['daemon']);
 
-  _onClose(code) {
-    if (process.env.DEBUG) {
-      console.log(`child process exited with code ${code}`)
-    }
-  }
-
-  _onData(data) {
-    const isReady = data.toString().includes('Daemon is ready');
+  ipfs.stdout.on('data', data => {
+    const string = data.toString();
+    const isReady = string.includes('Daemon is ready');
     if (isReady) {
-      succes('Daemon started');
-      this.emit('ready');
-      write(lockPath, true);
-    };
-  }
+      if (string.includes('Gateway')) {
+        const parts = string.split('Daemon is ready');
+        info(parts[0]);
+        succes('Daemon is ready');
+      } else {
+        succes(data);
+      }
+      emitter.emit('ready');
+    } else {
+      info(data);
+    }
+  });
 
-
-  _failsafe() {
-    fail('cannot acquire lock');
-    info('removing lock from repo');
-    const unlock = spawn(ipfsPath, ['repo', 'fsck'])
-    unlock.stdout.on('data', data => {
-      info('Restarting Daemon');
-      return this.start();
-    })
-  }
-
-  _onError(data) {
+  ipfs.stderr.on('data', data => {
     const string = data.toString();
     if (string.includes('cannot acquire lock')) {
-      this._failsafe();
+      fail('cannot acquire lock');
+      return _failsafe();
     } else {
       if (process.env.DEBUG) {
         console.log(string);
       }
-      this.emit('warning', string);
+      fail(data);
     }
-  }
+  });
+
+  ipfs.on('close', code => {
+    if (process.env.DEBUG) console.log(`child process exited with code ${code}`);
+  });
+};
+
+export const stop = () => {
+  log('killing Daemon');
+  ipfs.kill();
 }
